0384d17ac3ab67c884fbf0f6a330606ed78cbd65
MarkCarsonDev <markcarson.dev@gmail.com>
2025-01-22 19:23:44 -0800

    Footer and sidebar to sonne_vars

---
+++ sonne_sources/footer.py
@@ -0,0 +1,8 @@
+footer_html = '<div id="footer">\
+            <p>Designed intentionally to be lightweight.</p>\
+            <p>Efficiently compiled using <a href="https://github.com/MarkCarsonDev/Sonne">Sonne</a>, my static site generator.</a></p>\
+            <p>Kept dynamically static with an elegant but simple homemade CI/CD solution.</p>\
+            <p>Built from scratch in 2025 using simple HTML, JS, and CSS.</p>\
+        </div>'
+
+sonne_var("footer", footer_html)
---
+++ sonne_sources/sidebar.py
@@ -0,0 +1,14 @@
+sidebar_html = '<nav id="sidebar">\
+            <ul>\
+                <a href="#"><li id="kp1" class="keypress">Home</li></a>\
+                <a href="#about"><li id="kp2" class="keypress">About</li></a>\
+                <a href="#projects"><li id="kp3" class="keypress">Projects</li></a>\
+                <a href="#education"><li id="kp4" class="keypress">Education</li></a>\
+                <li class="listsep"></li>\
+                <a href="/blog"><li id="kpB" class="keypress">Blog</li></a>\
+                <a href="#contact"><li id="kpC" class="keypress">Contact</li></a>\
+\
+            </ul>\
+        </nav>'
+
+sonne_var("sidebar", sidebar_html)
