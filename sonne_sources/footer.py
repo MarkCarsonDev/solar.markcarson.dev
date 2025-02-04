db58825d135678aa30013dd395252e70e559dc0a
MarkCarsonDev <markcarson.dev@gmail.com>
2025-02-04 11:36:21 -0800

    Fixed embarrassing mistake :(

--- sonne_sources/footer.py
+++ sonne_sources/footer.py
@@ -5,7 +5,7 @@ footer_html = f'<div id="footer">\
             <p>Efficiently compiled using <a href="https://github.com/MarkCarsonDev/Sonne">Sonne</a>, my static site generator.</a></p>\
             <p>Kept dynamically static with an elegant but simple homemade CI/CD solution.</p>\
             <p>Built from scratch in 2025 using simple HTML, JS, and CSS.</p>\
-            <p>Compiled at {datetime.now()}<p>
+            <p>Compiled at {datetime.now()}<p>\
         </div>'
 
 sonne_var("footer", footer_html)
