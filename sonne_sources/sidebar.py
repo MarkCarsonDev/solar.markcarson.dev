f5533a3f3249a2bb78455ec0c75b67bd87abf553
MarkCarsonDev <markcarson.dev@gmail.com>
2025-02-04 12:17:50 -0800

    Added easier css targetting to sidebar list elements

--- sonne_sources/sidebar.py
+++ sonne_sources/sidebar.py
@@ -1,20 +1,20 @@
 sidebar_html = '<nav id="sidebar">\
             <ul>\
-                <a href="#"><li id="kp1" class="keypress">Home</li></a>\
-                <a href="#about"><li id="kp2" class="keypress">About</li></a>\
-                <a href="#projects"><li id="kp3" class="keypress">Projects</li></a>\
-                <a href="#education"><li id="kp4" class="keypress">Education</li></a>\
+                <a href="#"><li id="kp1" class="keypress"><span>Home</span></li></a>\
+                <a href="#about"><li id="kp2" class="keypress"><span>About</span></li></a>\
+                <a href="#projects"><li id="kp3" class="keypress"><span>Projects</span></li></a>\
+                <a href="#education"><li id="kp4" class="keypress"><span>Education</span></li></a>\
                 <li class="listsep"></li>\
-                <a href="/blog"><li id="kpB" class="keypress">Blog</li></a>\
-                <a href="/contact"><li id="kpC" class="keypress">Contact</li></a>\
+                <a href="/blog"><li id="kpB" class="keypress"><span>Blog</span></li></a>\
+                <a href="/contact"><li id="kpC" class="keypress"><span>Contact</span></li></a>\
 \
             </ul>\
         </nav>'
 
 sidebar_blog_html = '<nav id="sidebar">\
             <ul>\
-                <a href="/"><li id="kpH" class="keypress">Home</li></a>\
-                <a href="/contact"><li id="kpC" class="keypress">Contact</li></a>\
+                <a href="/"><li id="kpH" class="keypress"><span>Home</span></li></a>\
+                <a href="/contact"><li id="kpC" class="keypress"><span>Contact</span></li></a>\
 \
             </ul>\
         </nav>'
