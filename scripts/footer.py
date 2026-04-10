448dbcf3ef58f51096ef01e10d13d8bea0017d26
MarkCarsonDev <markcarson.dev@gmail.com>
2026-04-09 17:09:25 -0700

    Add timezone abbreviation to compiled timestamp

--- scripts/footer.py
+++ scripts/footer.py
@@ -5,7 +5,7 @@ la_time = datetime.now(zoneinfo.ZoneInfo("America/Los_Angeles"))
 
 footer_html = f'''<div id="footer">
     <span id="footer-solar">This site runs on an RPi Zero 2W &amp; a couple balcony solar panels.</span>
-    <span id="footer-build">Compiled {la_time.strftime("%Y-%m-%d %H:%M")}</span>
+    <span id="footer-build">Compiled {la_time.strftime("%Y-%m-%d %H:%M %Z")}</span>
 </div>'''
 
 sonne_var("footer", footer_html)
