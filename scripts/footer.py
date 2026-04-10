b9be40e0d99227ff2f0cabefb81ef511dc7e5193
MarkCarsonDev <markcarson.dev@gmail.com>
2026-04-09 17:00:15 -0700

    Remove content margin-top, bump footer opacity, fix RPi model name

--- scripts/footer.py
+++ scripts/footer.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 footer_html = f'''<div id="footer">
-    <span id="footer-solar">This site runs on an RPi 3b &amp; a couple balcony solar panels.</span>
+    <span id="footer-solar">This site runs on an RPi Zero 2W &amp; a couple balcony solar panels.</span>
     <span id="footer-build">Compiled {datetime.now().strftime("%Y-%m-%d %H:%M")}</span>
 </div>'''
 
