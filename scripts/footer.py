39ae1c9abb1a58815b2556fea1bd01b2043dd8a4
MarkCarsonDev <markcarson.dev@gmail.com>
2026-04-09 17:08:29 -0700

    Use LA timezone for compiled timestamp

--- scripts/footer.py
+++ scripts/footer.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+import zoneinfo
+
+la_time = datetime.now(zoneinfo.ZoneInfo("America/Los_Angeles"))
 
 footer_html = f'''<div id="footer">
     <span id="footer-solar">This site runs on an RPi Zero 2W &amp; a couple balcony solar panels.</span>
-    <span id="footer-build">Compiled {datetime.now().strftime("%Y-%m-%d %H:%M")}</span>
+    <span id="footer-build">Compiled {la_time.strftime("%Y-%m-%d %H:%M")}</span>
 </div>'''
 
 sonne_var("footer", footer_html)
